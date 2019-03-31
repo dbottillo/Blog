8503d5d7801b9ea2ce226961ba728188e15144ac
Daniele Bottillo <dbottillo@gmail.com>
2019-03-31 19:36:22 +0100

    indicator view blog post

--- app/src/main/java/com/danielebottillo/blog/MainActivity.kt
+++ app/src/main/java/com/danielebottillo/blog/MainActivity.kt
@@ -9,5 +9,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<IndicatorView>(R.id.indicator1).setOnClickListener {
+            findViewById<IndicatorView>(R.id.indicator1).colors = listOf(Color.BLACK, Color.RED)
+        }
+
+        findViewById<IndicatorView>(R.id.indicator1).colors = listOf(Color.BLUE)
+        findViewById<IndicatorView>(R.id.indicator2).colors = listOf(Color.BLACK, Color.RED)
+        findViewById<IndicatorView>(R.id.indicator3).colors = listOf(Color.WHITE, Color.RED, Color.GREEN)
+        findViewById<IndicatorView>(R.id.indicator4).colors = listOf(Color.BLUE, Color.BLACK, Color.RED, Color.GREEN)
+        findViewById<IndicatorView>(R.id.indicator5).colors = listOf(Color.WHITE, Color.BLUE, Color.BLACK, Color.RED, Color.GREEN)
     }
 }
