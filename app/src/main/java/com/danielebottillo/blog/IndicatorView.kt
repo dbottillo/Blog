ba06af7494e0b61be142da335492797d31670b5e
Daniele Bottillo <dbottillo@gmail.com>
2019-04-14 08:59:05 +0100

    indicator view animation

--- app/src/main/java/com/danielebottillo/blog/IndicatorView.kt
+++ app/src/main/java/com/danielebottillo/blog/IndicatorView.kt
@@ -1,5 +1,6 @@
 package com.danielebottillo.blog
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -7,6 +8,7 @@ import android.graphics.RectF
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 
 class IndicatorView @JvmOverloads constructor(
     context: Context,
@@ -24,8 +26,11 @@ class IndicatorView @JvmOverloads constructor(
             }
         }
 
+    private var animator: ValueAnimator? = null
+    private var currentSweepAngle = 0
+
     private val paint: Paint = Paint()
-    private val greyColor = ContextCompat.getColor(context, R.color.mtg_other)
+    private val greyColor = ContextCompat.getColor(context, R.color.white)
     private var arcs = emptyList<Arc>()
 
     private val colorMap = mapOf(Color.WHITE to ContextCompat.getColor(context, R.color.mtg_white),
@@ -44,7 +49,7 @@ class IndicatorView @JvmOverloads constructor(
                 Arc(start = startAngle, sweep = sweepSize, color = colorMap.getValue(color))
             }
         }
-        invalidate()
+        startAnimation()
     }
 
     init {
@@ -58,10 +63,38 @@ class IndicatorView @JvmOverloads constructor(
         rect.set(0f, 0f, w.toFloat(), h.toFloat())
     }
 
+    private fun startAnimation() {
+        animator?.cancel()
+        animator = ValueAnimator.ofInt(0, 360).apply {
+            duration = 650
+            interpolator = LinearInterpolator()
+            addUpdateListener { valueAnimator ->
+                currentSweepAngle = valueAnimator.animatedValue as Int
+                invalidate()
+            }
+        }
+        animator?.start()
+    }
+
     override fun onDraw(canvas: Canvas) {
         arcs.forEach { arc ->
-            paint.color = arc.color
-            canvas.drawArc(rect, START_ANGLE + arc.start, arc.sweep, true, paint)
+            if (currentSweepAngle > arc.start + arc.sweep) {
+                paint.color = arc.color
+                canvas.drawArc(rect,
+                    START_ANGLE + arc.start,
+                    arc.sweep,
+                    true,
+                    paint)
+            } else {
+                if (currentSweepAngle > arc.start) {
+                    paint.color = arc.color
+                    canvas.drawArc(rect,
+                        START_ANGLE + arc.start,
+                        currentSweepAngle - arc.start,
+                        true,
+                        paint)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/danielebottillo/blog/MainActivity.kt
+++ app/src/main/java/com/danielebottillo/blog/MainActivity.kt
@@ -10,14 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<IndicatorView>(R.id.indicator1).setOnClickListener {
-            findViewById<IndicatorView>(R.id.indicator1).colors = listOf(Color.BLACK, Color.RED)
+        findViewById<View>(R.id.start).setOnClickListener {
+            findViewById<IndicatorView>(R.id.indicator1).colors = listOf(Color.BLUE)
+            findViewById<IndicatorView>(R.id.indicator2).colors = listOf(Color.BLACK, Color.RED)
+            findViewById<IndicatorView>(R.id.indicator3).colors = listOf(Color.WHITE, Color.RED, Color.GREEN)
+            findViewById<IndicatorView>(R.id.indicator4).colors = listOf(Color.BLUE, Color.BLACK, Color.RED, Color.GREEN)
+            findViewById<IndicatorView>(R.id.indicator5).colors = listOf(Color.WHITE, Color.BLUE, Color.BLACK, Color.RED, Color.GREEN)
         }
-
-        findViewById<IndicatorView>(R.id.indicator1).colors = listOf(Color.BLUE)
-        findViewById<IndicatorView>(R.id.indicator2).colors = listOf(Color.BLACK, Color.RED)
-        findViewById<IndicatorView>(R.id.indicator3).colors = listOf(Color.WHITE, Color.RED, Color.GREEN)
-        findViewById<IndicatorView>(R.id.indicator4).colors = listOf(Color.BLUE, Color.BLACK, Color.RED, Color.GREEN)
-        findViewById<IndicatorView>(R.id.indicator5).colors = listOf(Color.WHITE, Color.BLUE, Color.BLACK, Color.RED, Color.GREEN)
     }
 }
